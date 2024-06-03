0bacfa918f6e7794f8a34274ca75741c301a9588
Renato Valer <valer9826@gmail.com>
2024-06-03 15:05:59 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,39 @@
+pluginManagement {
+    includeBuild("build-logic")
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+rootProject.name = "runnerApp"
+include(":app")
+include(":core:presentation:designsystem")
+include(":core:presentation:ui")
+include(":auth:data")
+include(":auth:presentation")
+include(":auth:domain")
+include(":core:data")
+include(":core:domain")
+include(":core:database")
+include(":run:data")
+include(":run:domain")
+include(":run:presentation")
+include(":run:location")
+include(":run:network")
